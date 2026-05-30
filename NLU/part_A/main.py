2710fdfea1abc68db4b947910c358ffc1b81abe1
Danilo Frailis <danilo.frailis@studenti.unipi.it>
2026-05-30 17:29:06 +0200

    .

--- NLU/part_A/main.py
+++ NLU/part_A/main.py
@@ -40,8 +40,8 @@ if __name__ == "__main__":
     lr = 0.005 #0.0005
     d_model = 128
     n_heads = 4
-    num_layer = [2, 4, 6]
-
+    num_layers = 2
+    ff_dims = [16, 32, 64, 128, 256]
 
 
     n_epochs = 200
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     slots_len = len(lang.id2slot) # pad & cls have the same id
     n_intents = len(lang.intent2id)
 
-    for num_layers in num_layer:
+    for ff_dim in ff_dims:
 
         slot_f1s, intent_acc = [], []
 
@@ -63,7 +63,7 @@ if __name__ == "__main__":
                 d_model=d_model,
                 n_heads=n_heads,
                 num_layers=num_layers,
-                ff_dim=20,
+                ff_dim=ff_dim,
             ).to(DEVICE)
             model.apply(init_weights)
 
@@ -86,7 +86,7 @@ if __name__ == "__main__":
             intent_acc.append(intent_test['accuracy'])
             slot_f1s.append(results_test['total']['f'])
 
-            PATH = os.path.join("bin", f"lr{lr}_dmodel{d_model}_nheads{n_heads}_nlayers{num_layers}_run{x}.pt")
+            PATH = os.path.join("bin", f"lr{lr}_dmodel{d_model}_nheads{n_heads}_nlayers{num_layers}_ffdim{ff_dim}_run{x}.pt")
             saving_object = {"epoch": x, 
                     "model": best_model, 
                     "optimizer": optimizer.state_dict(), 
